fe86efb894912278c0f22365b0695d25276ad24c
Rajat <66642921+rajat-1709@users.noreply.github.com>
2020-10-01 01:00:41 +0530

    Update main.py
    Modified  Version

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
-import cgi
-import subprocess
-
-print("content-type: text/html")
+#! /usr/bin/python3
+print("Content-type: text/html")
 print()
 
-mydata = cgi.FieldStorage()
+import cgi
+import subprocess
+import os
+import sys
 
-myx = mydata.getvalue("c")
 
+cmd = cgi.FieldStorage()
+value = cmd.getvalue('cmd')
 
-o = subprocess.getoutput("sudo " + myx)
+flag = True
 
-print("\n Hello User Wellcome to the output Screen")
-print("<br />")
-print("<br />")
+choice = value
+if ('date' in choice.lower()) or ('month' in choice.lower()) or ('year' in choice.lower()):
+	result = subprocess.getoutput('date')
+	print(f"Today's date is: {result}")
+	
+elif ('calender' in choice.lower()) or ('cal' in choice.lower()):
+	result = subprocess.getoutput('cal')
+	print(f"This Month Calender: {result}")
+	
+elif ('ifconfig' in choice.lower()) or ('what is my ip address' in choice.lower()):
+	result = subprocess.getoutput('ifconfig')
+	print(f"Your ip address is: {result}")
 
-print("..................................................................................................................................................................................................................................................................................................................................................................................................")
-print("<br />")
-print("<br />")
+elif ('view system profile' in choice.lower()) or ('show profile' in choice.lower()):
+	result = subprocess.getoutput('sosreport')
+	print(result)
 
-print("Your Command is : ",myx)
+elif ('configure system language' in choice.lower()) or ('language' in choice.lower()):
+	result = subprocess.getoutput('localectl')
+	print(result)
 
-print("<br />")
-print("<br />")
+elif ('configure network' in choice.lower()) or ('network' in choice.lower()):
+	result = subprocess.getoutput('nmcli')
+	print(result)
 
-print("..................................................................................................................................................................................................................................................................................................................................................................................................")
-print("<br />")
-print("<br />")
+elif ('configure hostname' in choice.lower()) or ('hostname' in choice.lower() or ('what is server hostname' in choice.lower()):
+	result = subprocess.getoutput('hostnamectl')
+	print(result)
 
-print("output is:",o)
+else:
+	print("we can't understand what you are asking")
